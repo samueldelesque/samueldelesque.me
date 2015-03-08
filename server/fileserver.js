c3cee14b0af91ec40702999d03c6dd83246e29b4
samueldelesque <contact@samueldelesque.com>
2015-03-08 15:00:00 -0400

    moved to node based http server

---
+++ server/fileserver.js
@@ -0,0 +1,11 @@
+var static = require('node-static')
+var file = new static.Server('./public')
+var port = 6134
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
+
+console.log("App files are now served on port "+port);
