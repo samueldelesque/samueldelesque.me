3ce1b896b33e7518be8bf006c4cecfeabbbe694e
Sam Delesque <sdelesque@domanistudios.com>
2015-05-20 16:10:49 -0400

    Folio update

--- public/app.js
+++ public/app.js
@@ -1,37 +1,45 @@
 (function(window,$){
 	$("html,body").css("scrollTop",0)
-	var w = $(window)
-	var body = $("body")
-	var els = {
-		intro: $("#intro"),
-		introTitle: $("#intro h3"),
-		first: $("#first"),
-		second: $("#second"),
-		third: $("#third"),
-		main: $("main"),
-		marker: $(".marker"),
-		logitems: $("#first .logitem"),
-		menuToggle: $("#menu-toggle")
-	}
+	var w = $(window),
+		body = $("body"),
+		els = {
+			intro: $("#intro"),
+			introTitle: $("#intro h3"),
+			first: $("#first"),
+			second: $("#second"),
+			third: $("#third"),
+			main: $("main"),
+			marker: $(".marker"),
+			logitems: $("#first .logitem"),
+			menuToggle: $("#menu-toggle"),
+			book: $(".book"),
+			page: $(".book figure"),
+			projects: $(".project")
+		}
 
 	els.menuToggle.on("click",function(){
 		body.toggleClass("active-menu")
 	})
 
-	$(".mailtoemail").attr("href","mailto:hello@"+"samuel"+"delesque.me?subject=Awesome work!")
+	var itemWidth,progressBar,mobileFolioWidth,mobileFolioOffset
 
-	var itemWidth = els.second.find("figure").outerWidth(true,true)-140,
+	w.on("resize",function(){
+		itemWidth = els.second.find("figure").outerWidth(true,true)-140,
 		progressBar = els.second.find(".wrap").siblings(".progress"),
 		mobileFolioWidth = els.second.find("figure").length * itemWidth - 500,
 		mobileFolioOffset = els.second.offset().top
-
+		
+		if(w.width() < 800) body.css("height","auto")
+		else body.css("height", mobileFolioOffset + mobileFolioWidth + els.third.outerHeight(true,true) + 1300);
+	})
+	w.trigger("resize")
 
 	w.scroll(function(e){
-		if(w.width() < 640) return;
+		if(w.width() < 800) return;
 
 		var top = w.scrollTop();
 
-		if(top < els.second.offset().top){
+		if(top < mobileFolioOffset){
 			els.second.find(".wrap").css("margin-left",0)
 			els.main.css({top:-top})
 			progressBar.css({width:"0%"})
@@ -47,23 +55,17 @@
 			els.main.css({top:-(top-mobileFolioWidth)})
 			progressBar.css({width:"100%"})
 		}
+	})
+	w.trigger("scroll")
 
-		// body.css("background-position","0 "+(-top/1.25)+"px")
-	});
 	var count = $("main figure").length;
 	
-	w.on("resize",function(){
-		if(w.width() < 640) body.css("height","auto")
-		else body.css("height", mobileFolioOffset + mobileFolioWidth + els.third.outerHeight(true,true) + 1300);
-	})
-	w.trigger("resize")
 
 	var l = 300;
 	$("main figure").each(function(i,e){
-		var zindex = count - i
-		var el = $(this)
+		var zindex = count - i,
+			el = $(this)
 		el.css({
-			// left: l + (i * l),
 			zIndex: zindex
 		}).data({
 			zindex: zindex
@@ -71,11 +73,16 @@
 		setTimeout(function(){el.addClass("show")}, 200 + (i * 100))
 	});
 	setTimeout(function(){
-		$("main #first").addClass("ready");
-	}, count * 100 + 200);
-	w.trigger("scroll");
+		els.first.addClass("ready")
+	}, count * 100 + 200)
 
-	$("#pay").on("click",function(e){$("#payment").fadeIn();});
-	$("#payment").on("click",function(){$("#payment").fadeOut();});
-	$("#payment .inner").on("click",function(e){e.stopPropagation();});
+	els.projects.on("click",function(e){
+		console.log($(this));
+		window.open($(this).find("a").attr("href"))
+	})
+
+	$("#pay").on("click",function(e){$("#payment").fadeIn()})
+	$("#payment").on("click",function(){$("#payment").fadeOut()})
+	$("#payment .inner").on("click",function(e){e.stopPropagation()})
+	$(".mailtoemail").attr("href","mailto:hello@"+"samuel"+"delesque.me?subject=Awesome work!")
 })(window,jQuery)
